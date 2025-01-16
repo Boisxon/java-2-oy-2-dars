aae9dd55d6639502cdab6ebb29b7f47c7ff5f237
Boisxon <boisxon357@gmail.com>
2025-01-16 14:59:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,192 @@
+function burger1() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 45000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger2() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 79000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger3() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 34000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger4() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 45000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger5() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 43000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger6() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 79000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger7() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 149000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger8() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 43000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger9() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 62000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger10() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 53000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger11() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 36000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
+function burger12() {
+    const zakaz = prompt("Nechta set zakaz qilasan");
+
+    if (zakaz === null || zakaz.trim() === "" || isNaN(zakaz) || zakaz <=0){
+        alert("Iltimos, to'gri miqdori kiriting.");
+        return;
+    }
+    const rezult = zakaz * 20000;
+    const isConfirmed = confirm(`${zakaz} te set ${rezult} so'm bo'ladi. Zakaz qilasizmi`);
+
+    if (isConfirmed) {
+        alert("Zakaz qabul qilindi!")
+    }else{
+        alert("Zakaz bekor qilindi!")
+    }
+}
